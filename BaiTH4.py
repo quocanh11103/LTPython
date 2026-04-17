0a08096f6536d2066c960b45415d509069ebf9be
quocanh11103 <quocanh11103@gmail.com>
2026-04-17 11:32:19 +0700

    Bai tap 4: dem chan le

---
+++ BaiTH4.py
@@ -0,0 +1,13 @@
+n=int(input("Nhap so nguyen duong n: "))
+so_le=0
+so_chan=0
+while n > 0:
+    digit = n  + 10
+    if digit % 2 == 0:
+        so_chan +=1
+    else:
+        so_le += 1
+    n //=10
+
+print("Số lượng số chẵn:", so_chan)
+print("Số lượng số lẻ:", so_le )

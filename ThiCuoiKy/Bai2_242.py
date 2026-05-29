01bf4d258fd221d70ce73c6fb5f7413e3aa1f2cd
quocanh11103 <quocanh11103@gmail.com>
2026-05-29 16:07:08 +0700

    Bài 2 bài thi kết thúc học phần HK3 môn Lập trình Python

--- ThiCuoiKy/Bai2_242.py
+++ ThiCuoiKy/Bai2_242.py
@@ -1,5 +1,5 @@
 import math
-
+#hàm in ra bảng cưu chương từ a-b (a<b) hoặc từ b-a(b<a)
 def bang_cuu_chuong(a, b):
     if a < b:
         for i in range(a, b + 1):
@@ -11,7 +11,7 @@ def bang_cuu_chuong(a, b):
             print(f"\nBảng cửu chương {i}")
             for j in range(1, 11):
                 print(f"{i} x {j} = {i*j}")
-
+#Hàm xử lý kiểm tra có phải số nguyên tố không
 def la_so_nguyen_to(n):
     if n < 2:
         return False
@@ -21,27 +21,27 @@ def la_so_nguyen_to(n):
             return False
 
     return True
-
+#In ra số nguyên tố
 def liet_ke_so_nguyen_to(n):
     print("Các số nguyên tố nhỏ hơn", n, "là:")
     for i in range(2, n):
         if la_so_nguyen_to(i):
             print(i, end=" ")
 
-
+#Hàm in và kiểm tra vừa là ước số vừa là số nguyên tố
 def uoc_so_nguyen_to(n):
     print("\nCác ước số nguyên tố của", n, "là:")
     for i in range(1, n + 1):
         if n % i == 0 and la_so_nguyen_to(i):
             print(i, end=" ")
 
-
+# hàm input thông tin
 a, b = map(int, input("Nhập số a, b cách nhau bằng dấu phẩy: ").split(","))
-
+# gọi hàm in bảng cửu chương
 bang_cuu_chuong(a, b)
-
+#Nhập và gọi hàm in ra các số nguyên tố
 n = int(input("\nNhập n để liệt kê số nguyên tố: "))
 liet_ke_so_nguyen_to(n)
-
+#nhập và gọi hàm in và kiểm tra vừa là ước số vừa là số nguyên tố 
 m = int(input("\nNhập số cần tìm ước số nguyên tố: "))
 uoc_so_nguyen_to(m)

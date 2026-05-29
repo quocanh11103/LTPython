e088da3bceb1d7d3f6708a8610d21339dd00af37
quocanh11103 <quocanh11103@gmail.com>
2026-05-29 16:10:51 +0700

    Bài 3 bài thi kết thúc học phần môn Lập trình Python

--- ThiCuoiKy/Bai3_242.py
+++ ThiCuoiKy/Bai3_242.py
@@ -1,3 +1,4 @@
+#Hàm lambda Nhập và kiểm tra có là bội số của 13 và 19 không
 kiem_tra = lambda n: n % 13 == 0 or n % 19 == 0
 
 n = int(input("Nhập số nguyên n: "))
@@ -7,7 +8,7 @@ if kiem_tra(n):
 else:
     print(n, "không là bội của 13 hoặc 19")
 
-
+#Hàm lambda kiểm tra tam giác
 tam_giac = lambda a, b, c: (
     "Không phải tam giác"
     if a + b <= c or a + c <= b or b + c <= a
@@ -21,9 +22,9 @@ tam_giac = lambda a, b, c: (
     or b*b + c*c == a*a
     else "Tam giác thường"
 )
-
+# Hàm nhập thông tin tam giác
 a = int(input("\nNhập cạnh a: "))
 b = int(input("Nhập cạnh b: "))
 c = int(input("Nhập cạnh c: "))
-
+#hàm xuất thông tin kiểm tra tam giác
 print("Kết quả:", tam_giac(a, b, c))

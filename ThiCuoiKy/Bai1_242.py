5c877020bdaeeedd62b6c8362d8a5307d578b29d
quocanh11103 <quocanh11103@gmail.com>
2026-05-29 15:58:53 +0700

    Bài 1 bài kiểm tra kết thúc học phần HK3 môn Lập trình Python

--- ThiCuoiKy/Bai1_242.py
+++ ThiCuoiKy/Bai1_242.py
@@ -1,10 +1,14 @@
+# Nhập thông tin xủ lý
 dai = float (input("Nhập chiều dài đáy hình khối chữ nhật: "));
 rong = float(input("Nhập chiều rộng đáy hình khối chữ nhật: "));
 cao = float(input("Nhập chiều cao đáy hình hộp chữ nhật: "));
-
 n=int(input("Số lượng số lẻ cần hiển thị : "));
+
+# Xử lý thông tin
 dientichday=dai * rong;
 thetichkhoi=dai * rong* cao;
+
+#Xuất thông tin ra màn hình
 print("Diện tích đáy hình chữ nhật = ",round(dientichday,n),"cm\u00b2");
 print("Thể tích hình khối = ",round(thetichkhoi,n),"cm\u00b3");
 
